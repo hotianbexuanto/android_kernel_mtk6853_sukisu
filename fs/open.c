05ca3c9e469537840c7645338afc197c154bf5ba
hotianbexuanto <1772720865@qq.com>
2025-04-29 15:40:12 +0800

    修补 ksu_hooks_4.14.patch

--- fs/open.c
+++ fs/open.c
@@ -359,6 +359,10 @@ extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int
 			                    int *flags);
 #endif
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
+			 int *flags);
+#endif
 /*
  * access() needs to use the real uid/gid, not the effective uid/gid.
  * We do this by temporarily clearing all FS-related capabilities and
@@ -373,6 +377,9 @@ SYSCALL_DEFINE3(faccessat, int, dfd, const char __user *, filename, int, mode)
 	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
+	#ifdef CONFIG_KSU
+	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
+	#endif
 
 #ifdef CONFIG_KSU
 	ksu_handle_faccessat(&dfd, &filename, &mode, NULL);
